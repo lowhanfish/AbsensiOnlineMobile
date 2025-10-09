57d5a6bf6c139705a2d785062602b16d82222494
lowhanfish <lowhanfish@gmail.com>
2025-10-09 11:43:25 +0800

    Added component to route

--- AbsensiKonsel/App.tsx
+++ AbsensiKonsel/App.tsx
@@ -5,20 +5,27 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import Login from './src/pages/Auth/Login';
+import Dashboard from './src/pages/Dashboard/dashboard';
+import Absensi from './src/pages/Absensi/Absensi';
+import AbsensiFaceRecognation from './src/pages/Absensi/AbsensiFaceRecognation';
+import Darurat from './src/pages/Darurat.jsx/Darurat';
+import DaruratDetail from './src/pages/Darurat.jsx/DaruratDetail';
+import DaruratForm from './src/pages/Darurat.jsx/DaruratForm';
+import Apel from './src/pages/Apel/Apel';
+import ApelDetail from './src/pages/Apel/ApelDetail';
+import Izin from './src/pages/Izin/Izin';
+import IzinForm from './src/pages/Izin/IzinForm';
+
+
+
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 }
