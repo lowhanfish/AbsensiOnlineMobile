70b89e527f4cbfbb340ffa9a6dcff7441a9758c8
lowhanfish <lowhanfish@gmail.com>
2026-01-12 22:30:00 +0800

    Added styling page Settings

--- AbsensiKonsel/src/pages/settings/Settings.tsx
+++ AbsensiKonsel/src/pages/settings/Settings.tsx
@@ -65,6 +65,157 @@ const Settings = () => {
 
 const styles = StyleSheet.create({
 
+    container: {
+        flex: 1,
+        minHeight: height,
+        paddingHorizontal: 16
+    },
+    textbg2: {
+        position: 'absolute',
+        top: 24,
+        left: 28,
+        right: 28,
+        height: 41
+    },
+    textform: {
+        marginTop: 9,
+        marginBottom: 5,
+        fontSize: 10
+    },
+    infoText: {
+        fontSize: 8,
+        color: '#6b6b6b',
+        lineHeight: 14
+    },
+    infoTextform: {
+        fontSize: 12,
+        fontWeight: '400',
+        color: '#ADADAD',
+        lineHeight: 14
+    },
+    fontTitle: {
+        fontSize: 24,
+        color: '#FFFFFF',
+        fontWeight: '400',
+        fontFamily: 'Audiowide-Regular',
+    },
+    textWrapper: {},
+    fakeInput: {
+        height: 45,
+        width: '100%',
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#E6E4EF',
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+    },
+    picker: {
+        height: 55,
+        width: '100%',
+        marginLeft: 12,
+        fontSize: 16,
+        ...Platform.select({
+            ios: { color: '#ADADAD' },
+            android: { color: '#ADADAD' },
+        }),
+    },
+    pickerText: {
+        fontSize: 16,
+        color: '#ADADAD'
+    },
+    inlinePicker: {
+        position: 'absolute',
+        top: 320,
+        left: 28,
+        width: 298,
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        overflow: 'hidden',
+        borderWidth: 1,
+        borderColor: '#E6E4EF',
+    },
+    textarea: {
+        height: 160,
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#E6E4EF',
+        backgroundColor: '#ffffff',
+        padding: 12,
+        textAlignVertical: 'top',
+        fontSize: 14,
+    },
+    fab: {
+        position: 'absolute',
+        bottom: 24,
+        right: 45,
+        backgroundColor: '#ffffff',
+        width: 61,
+        height: 61,
+        borderRadius: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 6,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        zIndex: 20,
+    },
+    inputRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 12,
+        height: '100%',
+    },
+    calendarIcon: {
+        width: 20,
+        height: 20,
+        opacity: 0.7,
+    },
+    documentPickerContainer: {
+        marginBottom: 10,
+    },
+    btnPick: {
+        backgroundColor: '#F5F5F9',
+        borderWidth: 1,
+        borderColor: '#E6E4EF',
+        borderStyle: 'dashed',
+        borderRadius: 8,
+        height: 45,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    btnPickText: {
+        color: '#7E59C9',
+        fontWeight: 'bold',
+    },
+    fileList: {
+        marginTop: 8,
+    },
+    fileItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 8,
+        borderRadius: 6,
+        marginBottom: 5,
+        borderWidth: 1,
+        borderColor: '#eee',
+    },
+    fileName: {
+        fontSize: 12,
+        color: '#555',
+        flex: 1,
+        marginRight: 10,
+    },
+    removeText: {
+        color: 'red',
+        fontWeight: 'bold',
+        paddingHorizontal: 5,
+    },
+
 });
 
 export default Settings;
